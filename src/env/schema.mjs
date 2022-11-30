a9379ccaac026fd9318e82b08c6c5802985d2258
Logan A. Morrison <loganmorrison99@gmail.com>
2022-11-29 23:08:41 -0800

    Removing unused env vars

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,12 +8,6 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
-  APPLE_CLIENT_ID: z.string(),
-  APPLE_CLIENT_SECRET: z.string(),
-  GOOGLE_CLIENT_ID: z.string(),
-  GOOGLE_CLIENT_SECRET: z.string(),
 });
 
 /**
