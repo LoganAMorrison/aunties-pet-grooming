f9cf961fa10702425976a40b296225e290f0aed0
Logan A. Morrison <loganmorrison99@gmail.com>
2022-12-01 00:45:56 -0800

    Adding analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,29 @@
 import { type AppType } from "next/app";
+import Script from "next/script";
 
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-2GEJ936XLK`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2GEJ936XLK');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
